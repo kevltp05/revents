9397a5ba6fc5edc48d49f0a5776097722e4e20fd
Kevltp05 <kevltp05@yahoo.com>
2019-06-17 16:56:49 -0500

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/layout/App.jsx';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 const rootEl = document.getElementById('root');
