57c393b679d8f364483e00ef5c2b571e1b3f716f
Kevltp05 <kevltp05@yahoo.com>
2019-06-21 15:30:24 -0500

    End of Section 6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,20 +1,41 @@
 import React, { Component, Fragment } from 'react';
-
+import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router-dom';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
-import { Container } from 'semantic-ui-react';
-
+import EventForm from '../../features/event/EventForm/EventForm';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
+import HomePage from '../../features/home/HomePage';
+ 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
+         <Route path="/" exact component={HomePage} />
+         <Route 
+         // Here we are saying that if the path has a / with anything
+         // after it we will render this other fragment with routes
+          path='/(.+)'
+          render={() => (
+           <Fragment>
+            <NavBar />
+            <Container className="main">
+              <Route path="/events" component={EventDashboard} />
+              <Route path="/event/:id" component={EventDetailedPage} />
+              <Route path="/people" component={PeopleDashboard} />
+              <Route path="/profile/:id" component={UserDetailedPage} />
+              <Route path="/settings" component={SettingsDashboard} />
+              <Route path="/createEvent" component={EventForm} />
+            </Container>
+          </Fragment>
+         )}
+      />
       </Fragment>
     );
   }
 }
-
+ 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/layout/App.jsx';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootEl = document.getElementById('root');
 
 let render = () => {
-    ReactDOM.render(<App />, rootEl);
+    ReactDOM.render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    , rootEl);
 };
 
 if (module.hot) {
@@ -17,8 +21,3 @@ if (module.hot) {
 };
 
 render();
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
